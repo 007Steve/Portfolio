7f7013ad4c6a8b76d35373ac02fb013143517e1e
007steve <stephen.plummer1.sp@gmail.com>
2021-05-08 10:23:05 -0400

    added home page

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Nav from '../components/Nav'
+import "../styles/Home.css";
+function Home() {
+    return (
+        <div>
+            <Nav/>
+            <div className="home__heroSection">
+                dg
+            </div>
+        </div>
+    )
+}
+
+export default Home
